8a10bb3b2726f7d2c097e26c6ea248f603ae19b1
thiacstaa <anthoniely.costa@nave.org.br>
2026-02-24 10:42:54 -0300

    Update exercicio02.js

--- exercicio02.js
+++ exercicio02.js
@@ -121,12 +121,12 @@
  * deixam de ser acessíveis
  */
 // Respostas
-/* 01: margarida */ let escopo01 = 76
-/* 02: jaiminho */  let escopo02 = 93
-/* 03: valentina */ let escopo03 = 114
-/* 04: maria */     let escopo04 = 83
-/* 05: jose */      let escopo05 = 86
-/* 06: belarmino */ let escopo06 = 113
+/* 01: margarida */ let escopo01 = 75
+/* 02: jaiminho */  let escopo02 = 92
+/* 03: valentina */ let escopo03 = 113
+/* 04: maria */     let escopo04 = 82
+/* 05: jose */      let escopo05 = 85
+/* 06: belarmino */ let escopo06 = 112
 
 /** Questões 07 a 12
  * No mesmo escopo da referência acima, indique com true (verdadeiro) ou false (falso)
@@ -135,17 +135,21 @@
 // Respostas
 /* O escopo de margarida consegue acessar o de joao */
 let escopo07 = true
+
 /* O escopo de enzo consegue acessar o de valentina */
 let escopo08 = false
+
 /* O escopo de valentina consegue acessar o de magda */
 let escopo09 = false
+
 /* O escopo de rosa consegue acessar o de violeta */
 let escopo10 = false
+
 /* O escopo de jaiminho consegue acessar o de joaquim */
 let escopo11 = true
+
 /* O escopo de baltazar consegue acessar o de onofre */
 let escopo12 = false
-
 /* NÃO MUDAR NADA DESTA LINHA EM DIANTE */
 export {
   escopo01,
